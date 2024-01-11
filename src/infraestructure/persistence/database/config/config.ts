be460fa6134e03aef98f82ed7102c6fc6bf8619b
Gabriel Dalseco <gabridal96@gmail.com>
2024-01-11 19:46:55 -0300

    chore: config conection database

--- src/infraestructure/persistence/database/config/config.ts
+++ src/infraestructure/persistence/database/config/config.ts
@@ -1,18 +1,21 @@
 import { Dialect, Options } from 'sequelize';
 
 // Config to use MySQL in project
-// const config: Options = {
-//   username: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || 'password',
-//   database: process.env.DB_NAME || 'rental_car_db',
-//   host: process.env.DB_HOST || 'localhost',
-//   port: Number(process.env.DB_PORT) || 3306,
-//   dialect: process.env.DIALECT as Dialect || 'mysql',
-// };
-
-const config: Options = {
-  dialect: 'sqlite',
-  storage: process.env.DB_STORAGE || './sqlite-database.db',  // Adiciona o caminho para o arquivo SQLite
+const configMySQL: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'rental_car_db',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: process.env.DIALECT as Dialect || 'mysql',
 };
+
+// Config to use SQLite in project
+const configSQLite: Options = {
+  dialect: 'sqlite',
+  storage: process.env.DB_STORAGE || './sqlite-database.db',
+};
+
+const config = process.env.NODE_ENV === 'test' ? configSQLite : configMySQL;
 
 export = config;
